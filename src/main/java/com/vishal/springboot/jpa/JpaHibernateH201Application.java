adedee6e64544f28a899d40a3c5a233c8ce91ce7
Vishal Thakur <vishalthakur107@gmail.com>
2025-12-29 08:17:34 +0530

    H2DB Demo

---
+++ src/main/java/com/vishal/springboot/jpa/JpaHibernateH201Application.java
@@ -0,0 +1,13 @@
+package com.vishal.springboot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateH201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHibernateH201Application.class, args);
+    }
+
+}
